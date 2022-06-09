68c5a15464e0ea3d17907f402e232876c86bab1e
mhbouop <106680229+mhbouop@users.noreply.github.com>
2022-06-09 14:58:59 +0200

    Update heat_model_elizabeth.m

--- heat_model_elizabeth.m
+++ heat_model_elizabeth.m
@@ -12,10 +12,7 @@ T_air = 293;                %Starting temperature air in [K]
 t = 0;                  %Time at the start [s]
 t_final=1200;                 %Time at end cycle [s]
 %% First order variables
-
-%This section contains the properties and constants of the materials, but
-%also fixed variables of the setup
-
+%This section contains the properties and constants of the materials, but also fixed variables of the setup
 %Copper tubing
 r_inner_cu = 0.004;     %Inner radius copper tube [m]
 r_outer_cu = 0.006;     %Outer radius copper tube [m]
@@ -25,7 +22,6 @@ epsilon_cu = 0.05;      %Emissivity copper [-]
 rho_cu = 8960;          %Density copper [kg/m^3]
 c_cu = 386;             %Specific heat copper [J/kg K]
 k_cu = 400;             %Thermal conductivity copper [W/(m K)]
-
 %PVC tubing
 hsv_pvc_d = 0.0022;     %Thickness of the HSV PCV [m]
 r_outer_pvc = 0.055;    %Outer radius PVC tube [m]
@@ -37,7 +33,6 @@ epsilon_pvc = 0.9;      %Emissivity PVC [-]
 rho_pvc = 1330;         %Density PVC [kg/m^3]
 k_pvc = 0.19;           %Thermal conductivity PVC [W/(m K)]
 d_air_endcap = 0.01;    %Distance between Kingspan Therma and the PVC endcap. Used to determine therma resistance
-
 %Polyurethane tubing
 r_inner_pur = 0.004;    %Inner radius polyurethane tube [m]
 r_outer_pur = 0.006;    %Outer radius polyurathane tube [m]
@@ -45,7 +40,6 @@ d_pur = 0.002;          %Wall Thickness polyurthane tube [m]
 length_pur = 6;         %Length polyurethane tube [m]
 epsilon_pur = 0.9;      %Emissivity polyurethane [-]
 k_pur = 0.13;           %Thermal conducitivity polyurethane [W/(m K)]
-
 %Aluminium plate
 A_al = 1.085;           %Area aluminium plate [m^2]
 epsilon_paint = 0.95;   %Emissivity black acrylic paint [-]
@@ -56,22 +50,18 @@ c_al = 900;             %Specific heat aluminium [J/kg K]
 %Water
 rho_water_20C = 998;    %Density water at 20C [kg/m^3]
 c_water = 4148;         %Specific heat water [J/kg K]
-
 %Air
 rho_air = 1.29;         %Density air at 20C [kg/m^3]
 c_air = 1006;           %Specific heat air [J/kg K]
 k_air = 0.5;             %Placeholder
-
 %General Setup Wood Properties
 k_wood = 0.12;       %Thermal Conductivity of Pinewood [W/m k]                       
 d_wood = 0.054;      %Wood border thcikness [m], simplification, in reality 2 mm air pocket within 
 A_wood = 0.3107;     %Inner Surface Area wood layer [m], slight simplification
-
 %Kingspan Insulation Properties
 ks_k = 0.022;     % Thermal Conductivity Kingspan insulation layer[W/(m*k)]
 ks_d = 0.04;      % thickness of Kingspan insulation layer [m]
 ks_spacer_d = 0.01; % Thickness of a single Kingspan spacer layer [m]
-
 %Other
 sigma = 5.67*10^-8;     %Stefan Boltzmann constant [W/(m^2 K^4)]
 flowrate = 3/60000;     %Flowrate pump [m^3/s]
@@ -120,14 +110,15 @@ pvc_ec_a = (r_outer_pvc)^2 * pi;                                                
 % Air Insulation Values
 % The following values, once again lifted from the earlier model, aim at extracting helpfull values for calculation of the resistive properties of air in between the HSV and its insulation.
 % These values are simplified estimations.
-    hsv_air_avgdist1 = ((0.010+0.00795)/2)*((length_pvc - (pvc_ec_inner_length * 2))/length_pvc) + ((0.0078+0.00575)/2)*((pvc_ec_inner_length * 2)/length_pvc); %Average distance of the heat reflector to the outer wall of the PVC body [m], USES PRE CALCULATED VALUES.  Accounts for the double layer PVC near the endcap, [m]
-    hsv_air_avg_a1 = length_pvc * (hsv_al_width - (2*(sind(18)*0.005)));                                                                                                                %Surface area at the average point between pvc and reflector [m^2], (TODO implement avgdist 1)
-    hsv_air_avgdist2 = 0.00905;                                                                                                                                                             %Average distance between reflector and kingspan therma plate [m]
-    hsv_air_avg_a2 = length_pvc * (hsv_al_width + (2*(sind(18)*0.005)));                                                                                                                %Surface are at the average point between reflector and kingspan therma [m^2], (TODO implement avgdist 2)
+hsv_air_avgdist1 = ((0.010+0.00795)/2)*((length_pvc - (pvc_ec_inner_length * 2))/length_pvc) + ((0.0078+0.00575)/2)*((pvc_ec_inner_length * 2)/length_pvc); %Average distance of the heat reflector to the outer wall of the PVC body [m], USES PRE CALCULATED VALUES.  Accounts for the double layer PVC near the endcap, [m]
+hsv_air_avg_a1 = length_pvc * (hsv_al_width - (2*(sind(18)*0.005)));                                                                                                                %Surface area at the average point between pvc and reflector [m^2], (TODO implement avgdist 1)
+hsv_air_avgdist2 = 0.00905;                                                                                                                                                             %Average distance between reflector and kingspan therma plate [m]
+hsv_air_avg_a2 = length_pvc * (hsv_al_width + (2*(sind(18)*0.005)));                                                                                                                %Surface are at the average point between reflector and kingspan therma [m^2], (TODO implement avgdist 2)
     
 %Aluminum Reflector Tape Properties
-    d_al_t = 0.0001;                            % Thickness of an aluminum reflector segment [m]
-    a_al_t = hsv_al_width * length_pvc;         % Area of a single heat reflector element. 10 are present in total [m^2]
+d_al_t = 0.0001;                            % Thickness of an aluminum reflector segment [m]
+a_al_t = hsv_al_width * length_pvc;         % Area of a single heat reflector element. 10 are present in total [m^2]
+    
 % Convective heat transfer coefficients
 h_air = 10;         %CVTH of still air [W/(m^2 K)], PLACEHOLDER
 h_water = 0.5;      %CVTH of water [W/(m^2 K)], PLACEHOLDER
@@ -221,8 +212,3 @@ xlim([0, t_final]);
 ylim([0, 2000]);
 xlabel('Time [s]');
 ylabel('Temperature [K]');
-
-
-
-
-   
